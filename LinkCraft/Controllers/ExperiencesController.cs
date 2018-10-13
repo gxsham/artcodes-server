c6b6c6ee315fe304721daf6fd91d92f137be6fd3
Dorin Balan <dbalan@itron.com>
2018-10-13 16:46:17 +0200

    Implementing Experience API

---
+++ LinkCraft/Controllers/ExperiencesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LinkCraft.Models;
+using LinkCraft.Data;
+
+namespace LinkCraft.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExperiencesController : ControllerBase
+    {
+        private readonly LinkCraftContext _context;
+
+        public ExperiencesController(LinkCraftContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Experiences
+        [HttpGet]
+        public IEnumerable<Experience> GetExperience()
+        {
+            return _context.Experience;
+        }
+
+        // GET: api/Experiences/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExperience([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var experience = await _context.Experience.FindAsync(id);
+
+            if (experience == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(experience);
+        }
+
+        // PUT: api/Experiences/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExperience([FromRoute] Guid id, [FromBody] Experience experience)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != experience.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(experience).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExperienceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Experiences
+        [HttpPost]
+        public async Task<IActionResult> PostExperience([FromBody] Experience experience)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Experience.Add(experience);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetExperience", new { id = experience.Id }, experience);
+        }
+
+        // DELETE: api/Experiences/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExperience([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var experience = await _context.Experience.FindAsync(id);
+            if (experience == null)
+            {
+                return NotFound();
+            }
+
+            _context.Experience.Remove(experience);
+            await _context.SaveChangesAsync();
+
+            return Ok(experience);
+        }
+
+        private bool ExperienceExists(Guid id)
+        {
+            return _context.Experience.Any(e => e.Id == id);
+        }
+    }
+}
